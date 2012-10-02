5b83991f12b565491e4ccdc51c99e72ae0468fa6
deepsweet <kir@soulshine.in>
2012-10-02 14:47:57 +0400

    plugins/removeEditorsNSData: fix removing ns'ed element w/o attrs

--- plugins/removeEditorsNSData.js
+++ plugins/removeEditorsNSData.js
@@ -17,15 +17,26 @@ var editorNamespaces = require('./_collections').editorNamespaces,
  */
 exports.removeEditorsNSData = function(item, params) {
 
-    if (item.isElem() && item.hasAttr()) {
+    if (item.isElem()) {
 
-        if (item.isElem('svg')) {
+        if (item.hasAttr()) {
 
+            if (item.isElem('svg')) {
+
+                item.eachAttr(function(attr) {
+                    if (attr.prefix === 'xmlns' && editorNamespaces.indexOf(attr.value) > -1) {
+                        prefixes.push(attr.local);
+
+                        // <svg xmlns:sodipodi="">
+                        item.removeAttr(attr.name);
+                    }
+                });
+
+            }
+
+            // <* sodipodi:*="">
             item.eachAttr(function(attr) {
-                if (attr.prefix === 'xmlns' && editorNamespaces.indexOf(attr.value) > -1) {
-                    prefixes.push(attr.local);
-
-                    // <svg xmlns:sodipodi="">
+                if (prefixes.indexOf(attr.prefix) > -1) {
                     item.removeAttr(attr.name);
                 }
             });
@@ -36,13 +47,6 @@ exports.removeEditorsNSData = function(item, params) {
         if (prefixes.indexOf(item.prefix) > -1) {
             return false;
         }
-
-        // <* sodipodi:*="">
-        item.eachAttr(function(attr) {
-            if (prefixes.indexOf(attr.prefix) > -1) {
-                item.removeAttr(attr.name);
-            }
-        });
 
     }
 
