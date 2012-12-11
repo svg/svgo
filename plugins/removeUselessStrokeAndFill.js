c1356af23751e0bb24f8cf77d97d4be028b876ac
deepsweet <kir@soulshine.in>
2012-12-11 13:07:57 +0200

    new plugin: remove useless  and  attrs (close #75)

---
+++ plugins/removeUselessStrokeAndFill.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var regStrokeProps = /^stroke/,
+    regFillProps = /^fill/;
+
+/**
+ * Remove useless stroke and fill attrs.
+ *
+ * @param {Object} item current iteration item
+ * @param {Object} params plugin params
+ * @return {Boolean} if false, item will be filtered out
+ *
+ * @author Kir Belevich
+ */
+exports.removeUselessStrokeAndFill = function(item, params) {
+
+    if (item.isElem()) {
+
+        // remove stroke*
+        if (
+            params.stroke &&
+            (!item.hasAttr('stroke') ||
+             item.hasAttr('stroke-opacity', '0') ||
+             item.hasAttr('stroke-width', '0')
+            )
+        ) {
+            item.eachAttr(function(attr) {
+                if (regStrokeProps.test(attr.name)) {
+                    item.removeAttr(attr.name);
+                }
+            });
+        }
+
+        // remove fill*
+        if (
+            params.fill &&
+            item.hasAttr('fill', 'none') ||
+            item.hasAttr('fill-opacity', '0')
+        ) {
+            item.eachAttr(function(attr) {
+                if (regFillProps.test(attr.name)) {
+                    item.removeAttr(attr.name);
+                }
+            });
+
+            item.addAttr({
+                name: 'fill',
+                value: 'none',
+                prefix: '',
+                local: 'fill'
+            });
+        }
+
+    }
+
+};
