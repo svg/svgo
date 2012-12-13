fbbd5b2ace95c6e17e6ad70f1b3a44f2f87961be
deepsweet <kir@soulshine.in>
2012-12-13 22:23:16 +0400

    plugins/removeDoctype: do not remove if custom XML entities presents (fix #77)

--- plugins/removeDoctype.js
+++ plugins/removeDoctype.js
@@ -14,6 +14,12 @@
  * <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
  * q"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
  *
+ * @example
+ * <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ * "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" [
+ *     <!-- an internal subset can be embedded here -->
+ * ]>
+ *
  * @param {Object} item current iteration item
  * @return {Boolean} if false, item will be filtered out
  *
@@ -21,6 +27,10 @@
  */
 exports.removeDoctype = function(item) {
 
-    return !item.doctype;
+    // remove doctype only if custom XML entities declaration block does not presents
+    // http://en.wikipedia.org/wiki/Document_Type_Definition#Entity_declarations
+    if (item.doctype && item.doctype.substr(-1) !== ']') {
+        return false;
+    }
 
 };
