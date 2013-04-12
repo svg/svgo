8429ffb53ca298ecb2bda96dd58327d115e91b40
deepsweet <kir@soulshine.in>
2013-04-12 13:24:21 +0300

    examples: remake

---
+++ examples/test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var FS = require('fs'),
+    PATH = require('path'),
+    SVGO = require('../lib/svgo'),
+    filepath = PATH.resolve(__dirname, 'test.svg'),
+    svgo = new SVGO(/*{ custom config object }*/);
+
+FS.readFile(filepath, 'utf8', function(err, data) {
+
+    if (err) {
+        throw err;
+    }
+
+    svgo.optimize(data, function(result) {
+
+        console.log(result);
+
+        // {
+        //     // optimized SVG data string
+        //     data: '<svg width="10" height="20">test</svg>'
+        //     // additional info such as width/height
+        //     info: {
+        //         width: '10',
+        //         height: '20'
+        //     }
+        // }
+
+    });
+
+});
