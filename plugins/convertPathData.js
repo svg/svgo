4c4ced858d2506c95b88c04cc885d34d74e34b0f
Kalashnikov Igor <igor.kalashnikov@me.com>
2014-01-02 23:04:35 +0400

    skip data concatenation for z instruction in collapseRepeated

--- plugins/convertPathData.js
+++ plugins/convertPathData.js
@@ -493,10 +493,10 @@ function collapseRepeated(path) {
             item.instruction === prev.instruction
         ) {
             // increase previous h or v data with current
-            if ((item.instruction === 'h' || item.instruction === 'v')&& (prev.data[0] >= 0) == (item.data[0] >= 0)) {
+            if ((item.instruction === 'h' || item.instruction === 'v') && (prev.data[0] >= 0) == (item.data[0] >= 0)) {
                 prev.data[0] += item.data[0];
-            // concat previous data with current
-            } else {
+            // concat previous data with current if it is not z
+            } else if (item.instruction !== 'z') {
                 prev.data = prev.data.concat(item.data);
             }
 
