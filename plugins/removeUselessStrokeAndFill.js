5a371546c332768cb7b82fb4cf355ebca8057a3f
Brad Cerasani <bradcerasani@gmail.com>
2014-10-30 10:49:56 -0500

    combined if statements for verifying non-default fill-rule

--- plugins/removeUselessStrokeAndFill.js
+++ plugins/removeUselessStrokeAndFill.js
@@ -47,15 +47,14 @@ exports.fn = function(item, params) {
             item.hasAttr('fill-opacity', '0')
         ) {
             item.eachAttr(function(attr) {
-                if (regFillProps.test(attr.name)) {
-                    if (
-                        attr.name !== 'fill-rule' ||
-                        (attr.name === 'fill-rule' &&
-                        attr.value === 'nonzero'
-                        )
-                    ) {
-                        item.removeAttr(attr.name);
-                    }
+                if (
+                    regFillProps.test(attr.name) &&
+                    (attr.name !== 'fill-rule' ||
+                    (attr.name === 'fill-rule' &&
+                    attr.value === 'nonzero')
+                    )
+                ) {
+                    item.removeAttr(attr.name);
                 }
             });
 
