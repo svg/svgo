73f7f3c18346c3fa354b3e9939dc971fcfa938d1
GreLI <grelimail@gmail.com>
2015-02-23 16:51:45 +0300

    Improved code for parsing CSS in attributes values

--- plugins/convertStyleToAttrs.js
+++ plugins/convertStyleToAttrs.js
@@ -6,11 +6,30 @@ exports.active = true;
 
 var EXTEND = require('whet.extend'),
     stylingProps = require('./_collections').stylingProps,
-    regDeclarationBlock = /\s*((?:[\w-]+|\\(?:[0-9a-f]{1,6}|.)\s?)*)\s*:\s*((?:[^'"();\\]+|\\(?:[0-9a-f]{1,6}|.)\s?|'(?:[^'\\\n\r]|\\(?:[0-9a-f]{1,6}|\r\n|.)\s?)*'|"(?:[^"\\\n\r]|\\(?:[0-9a-f]{1,6}|\r\n|.)\s?)*"|[\w-]+\((?:[^'"()]+|'(?:[^'\\\n\r]|\\(?:[0-9a-f]{1,6}|\r\n|.)\s?)*'|"(?:[^"\\\n\r]|\\(?:[0-9a-f]{1,6}|\r\n|.)\s?)*")*\))*|[^;]*)\s*(?:;\s*|$)/ig,
-    regCleanupStyle = /(:|;)\s+/g;
+    rEscape = '\\\\(?:[0-9a-f]{1,6}\\s?|\\r\\n|.)',                 // Like \" or \2051. Code points consume one space.
+    rAttr = '\\s*(' + g('[^:;\\\\]', rEscape) + '*?)\\s*',          // attribute name like ‘fill’
+    rSingleQuotes = "'(?:[^'\\n\\r\\\\]|" + rEscape + ")*?(?:'|$)", // string in single quotes: 'smth'
+    rQuotes = '"(?:[^"\\n\\r\\\\]|' + rEscape + ')*?(?:"|$)',       // string in double quotes: "smth"
+    rQuotedString = new RegExp('^' + g(rSingleQuotes, rQuotes) + '$'),
+
+    // Parentheses, E.g.: url(data:image/png;base64,iVBO...).
+    // ':' and ';' inside of it should be threated as is. (Just like in strings.)
+    rParenthesis = '\\(' + g('[^\'"()\\\\]+', rEscape, rSingleQuotes, rQuotes) + '*?' + '\\)',
+
+    // The value. It can have strings and parentheses (see above). Fallbacks to anything in case of unexpected input.
+    rValue = '\\s*(' + g('[^\'"();\\\\]+?', rEscape, rSingleQuotes, rQuotes, rParenthesis, '[^;]*?') + '*?' + ')',
+
+    // End of declaration. Spaces outside of capturing groups help to do natural trimming.
+    rDeclEnd = '\\s*(?:;\\s*|$)',
+
+    // Final RegExp to parse CSS declarations.
+    regDeclarationBlock = new RegExp(rAttr + ':' + rValue + rDeclEnd, 'ig'),
+
+    // Comments expression. Honors escape sequences and strings.
+    regStripComments = new RegExp(g(rEscape, rSingleQuotes, rQuotes, '/\\*[^]*\\*/'), 'ig');
 
 /**
- * Convert style in attributes.
+ * Convert style in attributes. Cleanups comments and illegal declarations (without colon) as a side effect.
  *
  * @example
  * <g style="fill:#000; color: #fff;">
@@ -35,6 +54,12 @@ exports.fn = function(item) {
             styles = [],
             attrs = {};
 
+        // Strip CSS comments preserving escape sequences and strings.
+        styleValue = styleValue.replace(regStripComments, function(match) {
+            return match[0] == '/' ? '' :
+                match[0] == '\\' && /[-g-z]/i.test(match[1]) ? match[1] : match;
+        })
+
         regDeclarationBlock.lastIndex = 0;
         for (var rule, decl; rule = regDeclarationBlock.exec(styleValue);) {
             decl = [rule[1], rule[2]];
@@ -45,9 +70,13 @@ exports.fn = function(item) {
         if (styles.length) {
 
             styles = styles.filter(function(style) {
-                if (style) {
-                    var prop = style[0].trim(),
-                        val = style[1].replace(/^[\'\"](.+)[\'\"]$/, '$1').trim();
+                if (style[0]) {
+                    var prop = style[0].toLowerCase(),
+                        val = style[1];
+
+                    if (rQuotedString.test(val)) {
+                        val = val.substring(1, val.length - 1);
+                    }
 
                     if (stylingProps.indexOf(prop) > -1) {
 
@@ -68,9 +97,7 @@ exports.fn = function(item) {
             EXTEND(item.attrs, attrs);
 
             if (styles.length) {
-                item.attr('style').value = styles.join(';')
-                    .replace(regCleanupStyle, '$1')
-                    .trim();
+                item.attr('style').value = styles.join(';');
             } else {
                 item.removeAttr('style');
             }
@@ -80,6 +107,10 @@ exports.fn = function(item) {
     }
 
 };
+
+function g() {
+    return '(?:' + Array.prototype.join.call(arguments, '|') + ')';
+}
 
 function stringifyDeclaration() {
     return this.join(':');
