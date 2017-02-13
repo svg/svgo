5844b8ae5aac40528a5bff07de99e9a60aedc399
strarsis <strarsis@gmail.com>
2017-02-13 05:10:20 +0100

    Add plugin + test.

---
+++ plugins/usedChars.js
@@ -0,0 +1,58 @@
+'use strict';
+
+exports.type = 'full';
+
+exports.active = false;
+
+exports.description = 'used text';
+
+var styleOrScript = ['style', 'script'];
+
+exports.fn = function(data, params) {
+
+
+    var texts = [];
+
+    /**
+     * Bananas!
+     *
+     * @param {Array} items input items
+     * @return {Array} output items
+     */
+    function monkeys(items) {
+        for (var i = 0; i < items.content.length; i++) {
+            var item = items.content[i];
+
+            if (item.isElem(styleOrScript)) {
+                continue;
+            }
+            if (item.isElem()) {
+                if(item.content && typeof item.content[0] && typeof item.content[0].text !== 'undefined') {
+                  texts.push(item.content[0].text);
+                }
+            }
+
+            // go deeper
+            if (item.content) {
+                monkeys(item);
+            }
+        }
+        return items;
+    }
+    monkeys(data);
+
+    var text  = texts.join('');
+    var chars = text.split('').filter(function(item, i, ar){ return ar.indexOf(item) === i; });
+    var charsStr = chars.join('');
+ 
+    var charsStrEsc = charsStr.replace(/'/g, "&#39;");
+    var svgElem = data.content[0];
+    svgElem.addAttr({
+      name:   'used-chars',
+      value:  charsStrEsc,
+      prefix: '',
+      local:  'used-chars'
+    });
+
+    return data;
+};
