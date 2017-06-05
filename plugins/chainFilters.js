ec33f1d03c196f6f31b652ecb76cc66201ea15b0
strarsis <strarsis@gmail.com>
2017-06-06 01:03:53 +0200

    Improve element-filter mapping. Improve code. Improve ocmments.

--- plugins/chainFilters.js
+++ plugins/chainFilters.js
@@ -32,7 +32,7 @@ var matchId = function(urlVal) {
 };
 
 /**
- * Chain filter elements using CSS filter(...) (Fix some browsers like Firefox).
+ * Chain filter elements using CSS filter(...) (Workaround for some browsers like Firefox).
  *
  * @param {Object} document document element
  * @param {Object} opts plugin params
@@ -41,82 +41,86 @@ var matchId = function(urlVal) {
  */
 exports.fn = function(document) {
 
-    // Collect <filter> elements and element that use filter by ID
-    var filterElementsWithId = new Map(),
-        elementsUsingFilterElementsWithId = [],
-
-        useFilterVal = '',
-        useFilterUrl = '',
-        useFilterId = '';
-
+    // Collect <filter> elements and elements that use a <filter> element by ID
+    var filterIds = new Set(),
+        elementsUsingFilterById = [];
     domWalker.preorder(document, function(node) {
 
         // <filter> elements
-        if(node.elem === 'filter') {
-          if(!node.hasAttr('id')) {
-            return; // skip if no ID attribute
-          }
+        if (node.elem === 'filter') {
+            if(!node.hasAttr('id')) {
+                return; // skip if no ID attribute
+            }
+            var filterElemId = node.attr('id').value;
 
-          filterElementsWithId.set(node.attr('id').value, node);
-          return; // done with <filter> element
+            if (filterIds.has(filterElemId)) {
+                console.warn('Warning: \'#' + filterElemId + '\' is used multiple times for <filter> elements.');
+                return; // skip if ID already added
+            }
+            filterIds.add(filterElemId);
+
+            return; // done with <filter> element
         }
 
 
         // elements that use a filter (filter attribute)
-        if(!node.hasAttr('filter')) {
-          return; // skip if no filter attribute
+        if (!node.hasAttr('filter')) {
+            return; // skip if no filter attribute
         }
 
-        useFilterVal  = node.attr('filter').value;
-        if(useFilterVal.length === 0) {
-          return; // skip if empty filter attribute
+        var useFilterVal  = node.attr('filter').value;
+        if (useFilterVal.length === 0) {
+            return; // skip if empty filter attribute
         }
 
-        useFilterUrl = matchUrl(useFilterVal);
-        if(!useFilterUrl) {
+        var useFilterUrl = matchUrl(useFilterVal);
+        if (!useFilterUrl) {
           return; // skip if no url(...) used
         }
 
-        useFilterId = matchId(useFilterUrl);
-        if(!useFilterId) {
-          return; // skip if no #id in url(...) used
+        var useFilterId = matchId(useFilterUrl);
+        if (!useFilterId) {
+            return; // skip if no #id in url(...) used
         }
 
-        elementsUsingFilterElementsWithId.push({
-          filterId: useFilterId,
-          node: node
+        elementsUsingFilterById.push({
+            filterId: useFilterId,
+            node:     node
         });
     });
-
-    if(filterElementsWithId.size === 0 || elementsUsingFilterElementsWithId.size === 0) {
-      return document; // No elements that use filter and/or no filter elements, skip this SVG.
+    if(filterIds.length === 0) {
+        return document; // No <filter> elements, skip this SVG.
     }
 
 
-    // Filter elements that are actually used by an element (by ID)
-    var usedFilterElementsWithId = new Map(),
-        element;
-    for(element of elementsUsingFilterElementsWithId) {
-      usedFilterElementsWithId.set(element.filterId, element.node);
+    // elements that use a <filter> element that actually exists
+    var elementsUsingExistingFilterById = elementsUsingFilterById.filter(function(element) {
+        var filterExists = filterIds.has(element.filterId);
+        if (!filterExists) {
+            console.warn('Warning: Element uses non-existing <filter> \'#' + element.filterId + '\', element skipped.');
+        }
+        return filterExists;
+    });
+
+    if(elementsUsingExistingFilterById.length === 0) {
+        return document; // No existing <filter> elements are used, skip this SVG.
     }
 
-    if(usedFilterElementsWithId.size === 0) {
-      return document; // No filter elements used at all, skip this SVG.
-    }
 
+    // Generate CSS class list + styles for the <filter> elements
+    var usedFilterIds = new Set(
+        elementsUsingExistingFilterById.map(function(element) {
+            return element.filterId;
+        })
+    );
 
-    // Generate CSS class list from filters +
-    // Generate CSS from class list
     var filterClasses = new Map(),
-        filterClassName = '',
-        filterClassesStyles = csstree.fromPlainObject({type:'StyleSheet', children: []}),
-        filterClassRuleObj = {};
-    
-    for (var filterId of usedFilterElementsWithId.keys()) {
-        filterClassName = camelCase('filter ' + filterId);
+        filterClassesStyles = csstree.fromPlainObject({type:'StyleSheet', children: []});
+    for (var filterId of usedFilterIds) {
+        var filterClassName = camelCase('filter ' + filterId);
         filterClasses.set(filterId, filterClassName);
 
-        filterClassRuleObj = {
+        var filterClassRuleObj = {
             type: 'Rule', 
             selector: {
                 type: 'SelectorList',
@@ -156,34 +160,37 @@ exports.fn = function(document) {
     }
 
 
-    // Add new style element with these filter classes styles
-    var svgElem = document.content[0];
+    if(!filterClassesStyles.children.isEmpty()) {
+        // Add new style element with these filter classes styles
+        var svgElem = document.content[0];
 
-    // New <style>
-    var styleFilterClasses = new document.constructor({
-        elem: 'style',
-        prefix: '',
-        local: 'style',
-        content: [] // has to be added in extra step
-    }, svgElem);
+        // New <style>
+        var styleFilterClasses = new document.constructor({
+            elem: 'style',
+            prefix: '',
+            local: 'style',
+            content: [] // has to be added in extra step
+        }, svgElem);
 
-    // New text content for <style>
-    var styleFilterClassesText = new document.constructor({
-        text: csstree.translate(filterClassesStyles)
-    }, styleFilterClasses);
-    // Add text content to <style>
-    styleFilterClasses.spliceContent(0, 0, styleFilterClassesText);
+        // New text content for <style>
+        var styleFilterClassesText = new document.constructor({
+            text: csstree.translate(filterClassesStyles)
+        }, styleFilterClasses);
+        // Add text content to <style>
+        styleFilterClasses.spliceContent(0, 0, styleFilterClassesText);
 
-    // Add new <style> to <svg>
-    svgElem.spliceContent(0, 0, styleFilterClasses);
+        // Add new <style> to <svg>
+        svgElem.spliceContent(0, 0, styleFilterClasses);
+    }
 
 
     // Assign filter-using classes to corresponding filter-using elements
     // Remove then redundant filter attribute.
-    for (element of elementsUsingFilterElementsWithId) {
+    for (var element of elementsUsingExistingFilterById) {
         element.node.removeAttr('filter');
         element.node.class.add(filterClasses.get(element.filterId));
     }
+
 
     return document;
 };
