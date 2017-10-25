eceb95c3105b3b31eb54d00c5e82adf624d2a4f8
Keegan Street <kestreet@deloitte.com.au>
2017-10-25 11:20:29 +1100

    Updated addElementsToSVGElement plugin to support attribute values, as well as attribute names.

--- plugins/addAttributesToSVGElement.js
+++ plugins/addAttributesToSVGElement.js
@@ -7,7 +7,7 @@ exports.active = false;
 exports.description = 'adds attributes to an outer <svg> element';
 
 var ENOCLS = `Error in plugin "addAttributesToSVGElement": absent parameters.
-It should have a list of classes in "attributes" or one "attribute".
+It should have a list of "attributes" or one "attribute".
 Config example:
 
 plugins:
@@ -16,7 +16,13 @@ plugins:
 
 plugins:
 - addAttributesToSVGElement:
-    attributes: ["mySvg", "size-big"]`;
+    attributes: ["mySvg", "size-big"]
+
+plugins:
+- addAttributesToSVGElement:
+    attributes:
+        - focusable: false
+        - data-image: icon`;
 
 /**
  * Add attributes to an outer <svg> element. Example config:
@@ -29,10 +35,16 @@ plugins:
  * - addAttributesToSVGElement:
  *     attributes: ['data-icon', 'data-disabled']
  *
+ * plugins:
+ * - addAttributesToSVGElement:
+ *     attributes:
+ *         - focusable: false
+ *         - data-image: icon
+ *
  * @author April Arcus
  */
 exports.fn = function(data, params) {
-    if (!params || !(Array.isArray(params.attributes) && params.attributes.some(String) || params.attribute)) {
+    if (!params || !(Array.isArray(params.attributes) || params.attribute)) {
         console.error(ENOCLS);
         return data;
     }
@@ -42,11 +54,24 @@ exports.fn = function(data, params) {
 
     if (svg.isElem('svg')) {
         attributes.forEach(function (attribute) {
-            if (!svg.hasAttr(attribute)) {
-                svg.addAttr({
-                    name: attribute,
-                    prefix: '',
-                    local: attribute
+            if (typeof attribute === 'string') {
+                if (!svg.hasAttr(attribute)) {
+                    svg.addAttr({
+                        name: attribute,
+                        prefix: '',
+                        local: attribute
+                    });
+                }
+            } else if (typeof attribute === 'object') {
+                Object.keys(attribute).forEach(function (key) {
+                    if (!svg.hasAttr(key)) {
+                        svg.addAttr({
+                            name: key,
+                            value: attribute[key],
+                            prefix: '',
+                            local: key
+                        });
+                    }
                 });
             }
         });
