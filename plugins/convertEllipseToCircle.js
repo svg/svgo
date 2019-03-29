3efd71f8c8120dcdf375aac1f9ad6aeaff151e6d
Taylor Hunt <taylor.hunt@kroger.com>
2019-03-29 16:18:49 -0400

    Comply with linter

--- plugins/convertEllipseToCircle.js
+++ plugins/convertEllipseToCircle.js
@@ -12,12 +12,11 @@ exports.description = 'converts non-eccentric <ellipse>s to <circle>s';
  * @see http://www.w3.org/TR/SVG/shapes.html
  *
  * @param {Object} item current iteration item
- * @param {Object} params plugin params
  * @return {Boolean} if false, item will be filtered out
  *
  * @author Taylor Hunt
  */
-exports.fn = function(item, params) {
+exports.fn = function(item) {
     if (item.isElem('ellipse')) {
       var rx = item.attr('rx').value || 0;
       var ry = item.attr('ry').value || 0;
