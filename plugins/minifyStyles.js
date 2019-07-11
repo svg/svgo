294711dccbb71c5966bdfb0f74611b6b0fcfae49
strarsis <strarsis@gmail.com>
2019-07-11 18:38:26 +0200

    Replace csso with cssnano.

--- plugins/minifyStyles.js
+++ plugins/minifyStyles.js
@@ -7,7 +7,7 @@ exports.active = true;
 exports.description = 'minifies styles and removes unused styles based on usage data';
 
 exports.params = {
-    // ... CSSO options goes here
+    // ... cssnano options goes here
 
     // additional 
     usage: {
@@ -18,143 +18,33 @@ exports.params = {
     }
 };
 
-var csso = require('csso');
+var cssTools = require('../lib/css-tools'),
+    postcss  = require('postcss'),
+    cssnano  = require('cssnano');
 
 /**
- * Minifies styles (<style> element + style attribute) using CSSO
+ * Minifies styles (<style> element + style attribute) using cssnano
  *
  * @author strarsis <strarsis@gmail.com>
  */
-exports.fn = function(ast, options) {
+exports.fn = function(document, options) {
     options = options || {};
 
-    var minifyOptionsForStylesheet = cloneObject(options);
-    var minifyOptionsForAttribute = cloneObject(options);
-    var elems = findStyleElems(ast);
+    /*var minifyForStylesheet = postcss([cssnano]).process().sync,
+        minifyForAttribute  = postcss([cssnano]).process().sync;*/
+    var minify = function(css) {
+        return postcss.sync([cssnano]);
+    };
 
-    minifyOptionsForStylesheet.usage = collectUsageData(ast, options);
-    minifyOptionsForAttribute.usage = null;
-
-    elems.forEach(function(elem) {
-        if (elem.isElem('style')) {
+    var styleEls = document.querySelectorAll('style');
+    if(styleEls) {
+        for(var styleEl of styleEls) {
             // <style> element
-            var styleCss = elem.content[0].text || elem.content[0].cdata || [];
-            var DATA = styleCss.indexOf('>') >= 0 || styleCss.indexOf('<') >= 0 ? 'cdata' : 'text';
-
-            elem.content[0][DATA] = csso.minify(styleCss, minifyOptionsForStylesheet).css;
-        } else {
-            // style attribute
-            var elemStyle = elem.attr('style').value;
-
-            elem.attr('style').value = csso.minifyBlock(elemStyle, minifyOptionsForAttribute).css;
+            var styleCss    = cssTools.getCssStr(styleEl);
+            var styleCssMin = minify(styleCss).css;
+            cssTools.setCssStr(styleEl, styleCssMin);
         }
-    });
+    }
 
-    return ast;
+    return document;
 };
-
-function cloneObject(obj) {
-    var result = {};
-
-    for (var key in obj) {
-        result[key] = obj[key];
-    }
-
-    return result;
-}
-
-function findStyleElems(ast) {
-
-    function walk(items, styles) {
-        for (var i = 0; i < items.content.length; i++) {
-            var item = items.content[i];
-
-            // go deeper
-            if (item.content) {
-                walk(item, styles);
-            }
-
-            if (item.isElem('style') && !item.isEmpty()) {
-                styles.push(item);
-            } else if (item.isElem() && item.hasAttr('style')) {
-                styles.push(item);
-            }
-        }
-
-        return styles;
-    }
-
-    return walk(ast, []);
-}
-
-function shouldFilter(options, name) {
-    if ('usage' in options === false) {
-        return true;
-    }
-
-    if (options.usage && name in options.usage === false) {
-        return true;
-    }
-
-    return Boolean(options.usage && options.usage[name]);
-}
-
-function collectUsageData(ast, options) {
-
-    function walk(items, usageData) {
-        for (var i = 0; i < items.content.length; i++) {
-            var item = items.content[i];
-
-            // go deeper
-            if (item.content) {
-                walk(item, usageData);
-            }
-
-            if (item.isElem('script')) {
-                safe = false;
-            }
-
-            if (item.isElem()) {
-                usageData.tags[item.elem] = true;
-
-                if (item.hasAttr('id')) {
-                    usageData.ids[item.attr('id').value] = true;
-                }
-
-                if (item.hasAttr('class')) {
-                    item.attr('class').value.replace(/^\s+|\s+$/g, '').split(/\s+/).forEach(function(className) {
-                        usageData.classes[className] = true;
-                    });
-                }
-
-                if (item.attrs && Object.keys(item.attrs).some(function(name) { return /^on/i.test(name); })) {
-                    safe = false;
-                }
-            }
-        }
-
-        return usageData;
-    }
-
-    var safe = true;
-    var usageData = {};
-    var hasData = false;
-    var rawData = walk(ast, {
-        ids: Object.create(null),
-        classes: Object.create(null),
-        tags: Object.create(null)
-    });
-
-    if (!safe && options.usage && options.usage.force) {
-        safe = true;
-    }
-
-    for (var key in rawData) {
-        if (shouldFilter(options, key)) {
-            usageData[key] = Object.keys(rawData[key]);
-            hasData = true;
-        }
-    }
-
-    return safe && hasData ? usageData : null;
-}
