8594b3a0491f00a8216ec7beced5e43ae748255f
strarsis <strarsis@gmail.com>
2020-09-05 01:04:56 +0200

    Add code to replace the `<symbol>` element with configurable wrapper element (`svg` is the default). Add tests for `<symbol>` elements as target. Add comments.

--- plugins/dereferenceUses.js
+++ plugins/dereferenceUses.js
@@ -9,11 +9,13 @@ exports.description = 'dereferences <use/> elements';
 
 exports.params = {
     keepHref: false, // keep (xlink:)href attributes
+
+    symbolContainer: 'svg' // browsers use <svg/> as container of <symbol/> content (`g` could also be used)
 };
 
 
-const overridingUseAttributes = [ 'x', 'y', 'width', 'height', 'href', 'xlink:href' ];
-
+const overridingUseAttrs = [ 'x', 'y', 'width', 'height', 'href', 'xlink:href' ];
+const hrefAttrs = [ 'href', 'xlink:href' ];
 
 /**
  * Dereferences <use> elements
@@ -37,34 +39,45 @@ exports.fn = function(document, options) {
 
     // replace <use/> with referenced node
     for (var useEl of useEls) {
+
+        // `href`/`xlink:href` value
         var hrefAttr = useEl.attr('href') ? useEl.attr('href') : useEl.attr('xlink:href');
         if(!hrefAttr || hrefAttr.value.length === 0) continue;
         var href = hrefAttr.value;
 
-
+        // look up referenced element
         var targetEl = document.querySelector(href);
         if(!targetEl) continue;
 
 
+        // clone referenced element for insertion
         var insertEl = targetEl.clone();
 
 
-        // Attribute inheritance
+        // Attribute inheritance of the dereferenced element
         // @see https://developer.mozilla.org/en-US/docs/Web/SVG/Element/use
         //   "Only the attributes x, y, width, height and href on the use element will override those set on the referenced element.
         //    However, any other attributes not set on the referenced element will be applied to the use element."
         useEl.eachAttr(function(attr) {
-            if(insertEl.hasAttr(attr.name) && !overridingUseAttributes.includes(attr.name)) return;
-            if(!options.keepHref && attr.name === ('href' || 'xlink:href')) return;
+            if(insertEl.hasAttr(attr.name) && !overridingUseAttrs.includes(attr.name)) return;
+            if(!options.keepHref && hrefAttrs.includes(attr.name)) return;
 
             insertEl.addAttr(attr);
         });
-        insertEl.removeAttr('id'); // only the original node is allowed to have this ID
+        insertEl.removeAttr('id'); // only the original node is allowed to have this ID (IDs must be unique)
 
 
         var useParentEl = useEl.parentNode;
-        var useElPosition = useParentEl.content.indexOf(useEl); // position of <use/> in parent
-        useParentEl.spliceContent(useElPosition, 1, insertEl); // replace <use/> with node
+        // position of <use/> in parent
+        var useElPosition = useParentEl.content.indexOf(useEl);
+
+        // <symbol/> elements are template elements (hence not visible),
+        // browsers would place a <symbol/> element as a different element
+        if(insertEl.isElem('symbol'))
+            insertEl.renameElem(options.symbolContainer);
+
+        // replace the <use/> element with the referenced element
+        useParentEl.spliceContent(useElPosition, 1, insertEl);
     }
 
     return document;
