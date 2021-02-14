45a38caff56d65ebc3a7d04ef77fc0a36d1a82ee
Bogdan Chadkin <trysound@yandex.ru>
2021-02-14 11:31:19 +0300

    Make optimize synchronous
    Ref https://github.com/svg/svgo/issues/1015
    
    Looks like `sax` is synchronous and we do not need to listen "end"
    event. This allows to avoid all callbacks and make `optimize` method
    synchronous.

--- test/plugins/_index.js
+++ test/plugins/_index.js
@@ -44,10 +44,9 @@ describe('plugins tests', function() {
                         js2svg  : { pretty: true }
                     });
 
-                    return svgo.optimize(orig, {path: file}).then(function(result) {
-                        //FIXME: results.data has a '\n' at the end while it should not
-                        normalize(result.data).should.be.equal(should);
-                    });
+                    const result = svgo.optimize(orig, {path: file});
+                    //FIXME: results.data has a '\n' at the end while it should not
+                    normalize(result.data).should.be.equal(should);
                 });
 
             });
