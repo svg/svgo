efa62c8e301935a66813f7c2449bfe385b7cba72
Bogdan Chadkin <trysound@yandex.ru>
2021-02-18 14:12:27 +0300

    Fix multipass (#1332)
    Ref https://github.com/svg/svgo/issues/1330 https://github.com/svg/svgo/issues/1148 https://github.com/svg/svgo/issues/1133 https://github.com/svg/svgo/pull/1227 https://github.com/svg/svgo/issues/985 https://github.com/svg/svgo/issues/943
    Took tests from https://github.com/svg/svgo/pull/1177

--- test/svgo/_index.js
+++ test/svgo/_index.js
@@ -1,42 +1,49 @@
 'use strict';
 
 const { expect } = require('chai');
-const FS = require('fs');
-const PATH = require('path');
-const EOL = require('os').EOL;
+const fs = require('fs');
+const path = require('path');
+const { EOL } = require('os');
+const { optimize, extendDefaultPlugins } = require('../../lib/svgo.js');
+
 const regEOL = new RegExp(EOL, 'g');
-const { optimize } = require('../../lib/svgo.js');
 
-describe('indentation', function() {
+const normalize = (file) => {
+  return file.trim().replace(regEOL, '\n');
+};
 
-    it('should create indent with 2 spaces', function(done) {
+const parseFixture = async file => {
+  const filepath = path.resolve(__dirname, file);
+  const content = await fs.promises.readFile(filepath, 'utf-8');
+  return normalize(content).split(/\s*@@@\s*/);
+};
 
-        var filepath = PATH.resolve(__dirname, './test.svg'),
-            svgo;
-
-        FS.readFile(filepath, 'utf8', function(err, data) {
-            if (err) {
-                throw err;
-            }
-
-            var splitted = normalize(data).split(/\s*@@@\s*/),
-                orig     = splitted[0],
-                should   = splitted[1];
-
-            const result = optimize(orig, {
-              path: filepath,
-              plugins : [],
-              js2svg  : { pretty: true, indent: 2 }
-            });
-            expect(normalize(result.data)).to.equal(should);
-            done();
-
-        });
-
-    });
-
+describe('svgo', () => {
+  it('should create indent with 2 spaces', async () => {
+    const [original, expected] = await parseFixture('test.svg');
+      const result = optimize(original, {
+        plugins: [],
+        js2svg: { pretty: true, indent: 2 },
+      });
+      expect(normalize(result.data)).to.equal(expected);
+  });
+  it('should run multiple times', async () => {
+    const [original, expected] = await parseFixture('multipass.svg');
+      const result = optimize(original, {
+        multipass: true,
+      });
+      expect(normalize(result.data)).to.equal(expected);
+  });
+  it('should pass multipass count to plugins', async () => {
+    const [original, expected] = await parseFixture('multipass-prefix-ids.svg');
+      const result = optimize(original, {
+        multipass: true,
+        plugins: extendDefaultPlugins([
+          {
+            name: 'prefixIds',
+          },
+        ]),
+      });
+      expect(normalize(result.data)).to.equal(expected);
+  });
 });
-
-function normalize(file) {
-    return file.trim().replace(regEOL, '\n');
-}
