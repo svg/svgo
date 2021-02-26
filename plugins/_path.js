bb3328d5b54d7215d5f192a2e0925258dbb860b2
Bogdan Chadkin <trysound@yandex.ru>
2021-02-26 18:06:03 +0300

    Implement new path parser
    Regexps are quite leaky for complex parsing. Regression tests caught
    a few issues related to path parser.
    
    In this diff I implemented the new spec-compliant parser which solves
    2 regression cases and covers many edge cases hard to handle with
    regexps.

--- plugins/_path.js
+++ plugins/_path.js
@@ -1,16 +1,8 @@
 'use strict';
 
-var rNumber = String.raw`[-+]?(?:\d*\.\d+|\d+\.?)(?:[eE][-+]?\d+)?\s*`,
-    rCommaWsp = String.raw`(?:\s,?\s*|,\s*)`,
-    rNumberCommaWsp = `(${rNumber})` + rCommaWsp,
-    rFlagCommaWsp = `([01])${rCommaWsp}?`,
-    rCoordinatePair = String.raw`(${rNumber})${rCommaWsp}?(${rNumber})`,
-    rArcSeq = (rNumberCommaWsp + '?').repeat(2) + rNumberCommaWsp + rFlagCommaWsp.repeat(2) + rCoordinatePair;
+const { parsePathData } = require('../lib/path.js');
 
-var regPathInstructions = /([MmLlHhVvCcSsQqTtAaZz])\s*/,
-    regCoordinateSequence = new RegExp(rNumber, 'g'),
-    regArcArgumentSequence = new RegExp(rArcSeq, 'g'),
-    regNumericValues = /[-+]?(\d*\.\d+|\d+\.?)(?:[eE][-+]?\d+)?/g,
+var regNumericValues = /[-+]?(\d*\.\d+|\d+\.?)(?:[eE][-+]?\d+)?/g,
     transform2js = require('./_transforms').transform2js,
     transformsMultiply = require('./_transforms').transformsMultiply,
     transformArc = require('./_transforms').transformArc,
@@ -29,77 +21,22 @@ var regPathInstructions = /([MmLlHhVvCcSsQqTtAaZz])\s*/,
  * @return {Array} output array
  */
 exports.path2js = function(path) {
-    if (path.pathJS) return path.pathJS;
-
-    var paramsLength = { // Number of parameters of every path command
-            H: 1, V: 1, M: 2, L: 2, T: 2, Q: 4, S: 4, C: 6, A: 7,
-            h: 1, v: 1, m: 2, l: 2, t: 2, q: 4, s: 4, c: 6, a: 7
-        },
-        pathData = [],   // JS representation of the path data
-        instruction, // current instruction context
-        startMoveto = false;
-
-    // splitting path string into array like ['M', '10 50', 'L', '20 30']
-    path.attr('d').value.split(regPathInstructions).forEach(function(data) {
-        if (!data) return;
-        if (!startMoveto) {
-            if (data == 'M' || data == 'm') {
-                startMoveto = true;
-            } else return;
-        }
-
-        // instruction item
-        if (regPathInstructions.test(data)) {
-            instruction = data;
-
-            // z - instruction w/o data
-            if (instruction == 'Z' || instruction == 'z') {
-                pathData.push({
-                    instruction: 'z'
-                });
-            }
-        // data item
-        } else {
-            if (instruction == 'A' || instruction == 'a') {
-                var newData = [];
-                for (var args; (args = regArcArgumentSequence.exec(data));) {
-                    for (var i = 1; i < args.length; i++) {
-                        newData.push(args[i]);
-                    }
-                }
-                data = newData;
-            } else {
-                data = data.match(regCoordinateSequence);
-            }
-            if (!data) return;
-
-            data = data.map(Number);
-            // Subsequent moveto pairs of coordinates are threated as implicit lineto commands
-            // https://www.w3.org/TR/SVG11/paths.html#PathDataMovetoCommands
-            if (instruction == 'M' || instruction == 'm') {
-                pathData.push({
-                    instruction: pathData.length == 0 ? 'M' : instruction,
-                    data: data.splice(0, 2)
-                });
-                instruction = instruction == 'M' ? 'L' : 'l';
-            }
-
-            for (var pair = paramsLength[instruction]; data.length;) {
-                pathData.push({
-                    instruction: instruction,
-                    data: data.splice(0, pair)
-                });
-            }
-        }
-    });
-
-    // First moveto is actually absolute. Subsequent coordinates were separated above.
-    if (pathData.length && pathData[0].instruction == 'm') {
-        pathData[0].instruction = 'M';
+  if (path.pathJS) return path.pathJS;
+  const pathData = [];   // JS representation of the path data
+  const commands = parsePathData(path.attr('d').value);
+  for (const { name, args } of commands) {
+    if (name === 'Z' || name === 'z') {
+      pathData.push({ instruction: 'z' });
+    } else {
+      pathData.push({ instruction: name, data: args });
     }
-    path.pathJS = pathData;
-
-    return pathData;
+  }
+  // First moveto is actually absolute. Subsequent coordinates were separated above.
+  if (pathData.length && pathData[0].instruction == 'm') {
+    pathData[0].instruction = 'M';
+  }
+  path.pathJS = pathData;
+  return pathData;
 };
 
 /**
