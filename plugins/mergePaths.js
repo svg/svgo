de4fd79b5779a2e92f45dd06685cec5192067ecd
Bogdan Chadkin <trysound@yandex.ru>
2021-03-05 23:59:27 +0300

    Prevent merging path when marker-end style is specified
    Ref https://github.com/svg/svgo/issues/1217 https://github.com/svg/svgo/issues/958 https://github.com/svg/svgo/issues/872

--- plugins/mergePaths.js
+++ plugins/mergePaths.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const { computeStyle } = require('../lib/style.js');
+const { path2js, js2path, intersects } = require('./_path.js');
+
 exports.type = 'perItem';
 
 exports.active = true;
@@ -7,16 +10,12 @@ exports.active = true;
 exports.description = 'merges multiple paths in one if possible';
 
 exports.params = {
-    collapseRepeated: true,
-    force: false,
-    leadingZero: true,
-    negativeExtraSpace: true,
-    noSpaceAfterFlags: false, // a20 60 45 0 1 30 20 → a20 60 45 0130 20
+  collapseRepeated: true,
+  force: false,
+  leadingZero: true,
+  negativeExtraSpace: true,
+  noSpaceAfterFlags: false, // a20 60 45 0 1 30 20 → a20 60 45 0130 20
 };
-
-var path2js = require('./_path.js').path2js,
-    js2path = require('./_path.js').js2path,
-    intersects = require('./_path.js').intersects;
 
 /**
  * Merge multiple Paths into one.
@@ -26,48 +25,56 @@ var path2js = require('./_path.js').path2js,
  *
  * @author Kir Belevich, Lev Solntsev
  */
-exports.fn = function(item, params) {
+exports.fn = function (item, params) {
+  if (!item.isElem() || item.isEmpty()) return;
 
-    if (!item.isElem() || item.isEmpty()) return;
+  var prevContentItem = null,
+    prevContentItemKeys = null;
 
-    var prevContentItem = null,
-        prevContentItemKeys = null;
-
-    item.content = item.content.filter(function(contentItem) {
-
-        if (prevContentItem &&
-            prevContentItem.isElem('path') &&
-            prevContentItem.isEmpty() &&
-            prevContentItem.hasAttr('d') &&
-            contentItem.isElem('path') &&
-            contentItem.isEmpty() &&
-            contentItem.hasAttr('d')
-        ) {
-
-            if (!prevContentItemKeys) {
-                prevContentItemKeys = Object.keys(prevContentItem.attrs);
-            }
-
-            var contentItemAttrs = Object.keys(contentItem.attrs),
-                equalData = prevContentItemKeys.length == contentItemAttrs.length &&
-                    contentItemAttrs.every(function(key) {
-                        return key == 'd' ||
-                            prevContentItem.hasAttr(key) &&
-                            prevContentItem.attr(key).value == contentItem.attr(key).value;
-                    }),
-                prevPathJS = path2js(prevContentItem),
-                curPathJS = path2js(contentItem);
-
-            if (equalData && (params.force || !intersects(prevPathJS, curPathJS))) {
-                js2path(prevContentItem, prevPathJS.concat(curPathJS), params);
-                return false;
-            }
-        }
-
-        prevContentItem = contentItem;
-        prevContentItemKeys = null;
+  item.content = item.content.filter(function (contentItem) {
+    if (
+      prevContentItem &&
+      prevContentItem.isElem('path') &&
+      prevContentItem.isEmpty() &&
+      prevContentItem.hasAttr('d') &&
+      contentItem.isElem('path') &&
+      contentItem.isEmpty() &&
+      contentItem.hasAttr('d')
+    ) {
+      const computedStyle = computeStyle(contentItem);
+      // keep path to not break markers
+      if (
+        computedStyle['marker-start'] ||
+        computedStyle['marker-mid'] ||
+        computedStyle['marker-end']
+      ) {
         return true;
+      }
+      if (!prevContentItemKeys) {
+        prevContentItemKeys = Object.keys(prevContentItem.attrs);
+      }
 
-    });
+      var contentItemAttrs = Object.keys(contentItem.attrs),
+        equalData =
+          prevContentItemKeys.length == contentItemAttrs.length &&
+          contentItemAttrs.every(function (key) {
+            return (
+              key == 'd' ||
+              (prevContentItem.hasAttr(key) &&
+                prevContentItem.attr(key).value == contentItem.attr(key).value)
+            );
+          }),
+        prevPathJS = path2js(prevContentItem),
+        curPathJS = path2js(contentItem);
 
+      if (equalData && (params.force || !intersects(prevPathJS, curPathJS))) {
+        js2path(prevContentItem, prevPathJS.concat(curPathJS), params);
+        return false;
+      }
+    }
+
+    prevContentItem = contentItem;
+    prevContentItemKeys = null;
+    return true;
+  });
 };
