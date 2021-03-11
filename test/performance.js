ddd01d0bf302c634805bc9541a7854d97de13178
Bogdan Chadkin <trysound@yandex.ru>
2021-03-11 14:31:27 +0300

    Implement correct fibonacci measuring

--- test/performance.js
+++ test/performance.js
@@ -4,20 +4,23 @@ const fs = require('fs');
 const path = require('path');
 const { optimize } = require('../');
 
-const measureFibonacci = (count) => {
+const measureFibonacci = () => {
   const start = process.hrtime.bigint();
-  const fib = Array(count);
-  let i;
-  fib[0] = 0;
-  fib[1] = 1;
-  for (i = 2; i <= count; i += 1) {
-    fib[i] = fib[i - 2] + fib[i - 1];
+  const count = 1000;
+  const times = 500_000;
+  for (let k = 0; k < times; k += 1) {
+    const fib = Array(count);
+    let i;
+    fib[0] = 0;
+    fib[1] = 1;
+    for (i = 2; i <= count; i += 1) {
+      fib[i] = fib[i - 2] + fib[i - 1];
+    }
   }
   const end = process.hrtime.bigint();
   const diff = end - start;
-  console.info(
-    `Computed ${count} fibonacci numbers in ${diff / BigInt(1e6)}ms`
-  );
+  const ms = diff / BigInt(1e6);
+  console.info(`Computed ${count} fibonacci numbers ${times} times in ${ms}ms`);
   return diff;
 };
 
@@ -30,11 +33,12 @@ const measureOptimize = (fixturePath) => {
   }
   const end = process.hrtime.bigint();
   const diff = end - start;
-  console.info(`Optimized svg in ${diff / BigInt(1e6)}ms`);
+  const ms = diff / BigInt(1e6);
+  console.info(`Optimized svg in ${ms}ms`);
   return diff;
 };
 
-const baseTime = measureFibonacci(40_000_000);
+const baseTime = measureFibonacci();
 const fixturesDir = path.join(__dirname, 'performance-fixtures');
 const snapshot = JSON.parse(
   fs.readFileSync(path.join(fixturesDir, 'snapshot.json'), 'utf-8')
@@ -45,7 +49,8 @@ for (const [file, ratio] of Object.entries(snapshot)) {
   const optimisationTime = measureOptimize(path.join(fixturesDir, file));
   const newRatio = Number(optimisationTime / baseTime);
   const percentile = Math.abs((newRatio / ratio) * 100 - 100);
-  if (percentile > 10) {
+  const maxPercentile = 10;
+  if (percentile > maxPercentile) {
     errors.push(
       `${file} has ${newRatio} ratio instead of ${ratio} in snapshot`
     );
