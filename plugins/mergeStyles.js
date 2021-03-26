b44469d80fffc369e2f9d57395ea60185504e8b3
strarsis <strarsis@gmail.com>
2021-03-26 19:15:27 +0100

    Improve symbol names (full name).

--- plugins/mergeStyles.js
+++ plugins/mergeStyles.js
@@ -16,27 +16,27 @@ exports.description = 'merge multiple style elements into one';
  */
 exports.fn = function (document) {
   // collect <style/>s (preserve order)
-  const styleEls = querySelectorAll(document, 'style');
+  const styleElements = querySelectorAll(document, 'style');
 
   // no <styles/>s, nothing to do
-  if (styleEls.length <= 1) {
+  if (styleElements.length <= 1) {
     return document;
   }
 
   let styles = [];
-  for (let styleEl of styleEls) {
-    if (closestByName(styleEl, 'foreignObject')) {
+  for (let styleElement of styleElements) {
+    if (closestByName(styleElement, 'foreignObject')) {
       // skip <foreignObject> content
       continue;
     }
 
-    const cssStr = getCssStr(styleEl);
+    const cssString = getCssStr(styleElement);
 
     styles.push({
-      styleEl: styleEl,
+      styleElement: styleElement,
 
-      mq: styleEl.attributes.media,
-      cssStr: cssStr,
+      mq: styleElement.attributes.media,
+      cssStr: cssString,
     });
   }
 
@@ -54,28 +54,27 @@ exports.fn = function (document) {
 
     // remove all processed style elements – except the first one
     if (styleNo > 0) {
-      removeFromParent(style.styleEl);
+      removeFromParent(style.styleElement);
     }
   }
 
   // re-assign the collected styles to the first style element
   let firstStyle = styles[0];
-  delete firstStyle.styleEl.attributes.media; // remove media mq attribute as CSS media queries are used
-  const collectedStylesStr = collectedStyles.join('');
-  if(collectedStylesStr.trim().length > 0) {
-    setCssStr(firstStyle.styleEl, collectedStylesStr);
+  delete firstStyle.styleElement.attributes.media; // remove media mq attribute as CSS media queries are used
+  const collectedStylesString = collectedStyles.join('');
+  if (collectedStylesString.trim().length > 0) {
+    setCssStr(firstStyle.styleElement, collectedStylesString);
   } else {
-    removeFromParent(firstStyle.styleEl);
+    removeFromParent(firstStyle.styleElement);
   }
 
   return document;
 };
 
-
-function removeFromParent(el) {
-  const parentEl = el.parentNode;
-  return parentEl.children.splice(
-	parentEl.children.indexOf(el),
-	1
+function removeFromParent(element) {
+  const parentElement = element.parentNode;
+  return parentElement.children.splice(
+    parentElement.children.indexOf(element),
+    1
   );
 }
