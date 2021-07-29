b8d8fa3e93103bb2811b7623e163127e84f53185
antoni <antoni.rosciszewskI@gmail.com>
2021-07-29 08:56:38 +0200

    feat: Add SVG validation/linting

---
+++ pluginsValidate/hasCorrectStripeColors.js
@@ -0,0 +1,67 @@
+'use strict';
+
+exports.type = 'validate';
+
+exports.active = true;
+
+exports.description = 'checks if color stripes are in correct';
+
+const utils = require('./validationUtilities.js');
+
+const ENOCLS = `Error in plugin "hasCorrectStripeColors": absent parameters.
+It should have a ordered list of stripe colors in "stripeColors" attribute.
+Config example:
+
+plugins:
+-stripeColors: [
+    'blackFill',
+    'blackStroke',
+    'whiteFill',
+    'stripes',
+    'darkmodeMask',
+  ]
+`;
+
+/**
+ * Checks if color stripes are in correct
+ *
+ * @example
+ *
+ * @param {Object} root current iteration root
+ * @param {Object} validateResult current validation result
+ *
+ * @return {Object} validateResult holding the validation result
+ *
+ * @author Tymon Żarski
+ */
+exports.fn = function (root, validateResult, params) {
+  let result = false;
+  if (params.stripeColors !== undefined) {
+    const stripeColorsOrder = params.stripeColors;
+    const allElements = utils.findFillElementsByColors(root, stripeColorsOrder);
+
+    result = fillColorOrderCorrect(allElements, stripeColorsOrder);
+  } else {
+    console.error(ENOCLS);
+  }
+
+  validateResult.hasCorrectStripeColors = result;
+
+  return validateResult;
+};
+
+// check if values have the same order in the arrays
+function fillColorOrderCorrect(stripeElements, stripeElementColorOrder) {
+  if (stripeElements.length !== stripeElementColorOrder.length) {
+    return false;
+  }
+  for (let i = 0; i < stripeElementColorOrder.length; i++) {
+    if (
+      stripeElements[i].attributes.fill.toLowerCase() !==
+      stripeElementColorOrder[i].toLowerCase()
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
---
+++ pluginsValidate/isPrefixPresent.js
@@ -0,0 +1,41 @@
+'use strict';
+
+exports.type = 'validate';
+
+exports.active = false;
+
+exports.description = 'checks if the filename has prefix';
+
+/**
+ * Checks if the file is snake_case named
+ *
+ * @example
+ *
+ * @param {Object} root current iteration root
+ * @param {Object} validateResult current validation result
+ *
+ * @return {Object} validateResult holding the validation result
+ *
+ * @author Tymon Żarski
+ */
+
+exports.fn = function (root, validateResult) {
+  if (root.filename) {
+    const filename = root.filename;
+    const prefixes = [
+      'retail_',
+      'corporate_',
+      'sme_',
+      'privatebanking_',
+      'mobile_',
+    ];
+    const result = prefixes.some((prefix) => filename.indexOf(prefix) === 0);
+
+    validateResult.isPrefixPresent = result;
+  } else {
+    validateResult.isPrefixPresent = false;
+    console.log('no filename provided!');
+  }
+
+  return validateResult;
+};
