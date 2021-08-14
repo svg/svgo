9b7a59e2dc5f6127a4d8dcf3fdee6e364c264311
Bogdan Chadkin <trysound@yandex.ru>
2021-08-14 17:20:31 +0300

    Add plugin types

---
+++ lib/types.ts
@@ -0,0 +1,74 @@
+type XastDoctype = {
+  type: 'doctype';
+  name: string;
+  data: {
+    doctype: string;
+  };
+};
+
+type XastInstruction = {
+  type: 'instruction';
+  name: string;
+  value: string;
+};
+
+type XastComment = {
+  type: 'comment';
+  value: string;
+};
+
+type XastCdata = {
+  type: 'cdata';
+  value: string;
+};
+
+type XastText = {
+  type: 'text';
+  value: string;
+};
+
+type XastElement = {
+  type: 'element';
+  name: string;
+  attributes: Record<string, string>;
+  children: Array<XastChild>;
+};
+
+export type XastChild =
+  | XastDoctype
+  | XastInstruction
+  | XastComment
+  | XastCdata
+  | XastText
+  | XastElement;
+
+type XastRoot = {
+  type: 'root';
+  children: Array<XastChild>;
+};
+
+export type XastParent = XastRoot | XastElement;
+
+export type XastNode = XastRoot | XastChild;
+
+type VisitorNode<Node> = {
+  enter?: (node: Node, parentNode: XastParent) => void;
+  leave?: (node: Node, parentNode: XastParent) => void;
+};
+
+type VisitorRoot = {
+  enter?: (node: XastRoot, parentNode: null) => void;
+  leave?: (node: XastRoot, parentNode: null) => void;
+};
+
+export type Visitor = {
+  doctype?: VisitorNode<XastDoctype>;
+  instruction?: VisitorNode<XastInstruction>;
+  comment?: VisitorNode<XastComment>;
+  cdata?: VisitorNode<XastCdata>;
+  text?: VisitorNode<XastText>;
+  element?: VisitorNode<XastElement>;
+  root?: VisitorRoot;
+};
+
+export type Plugin<Params> = (root: XastRoot, params: Params) => Visitor;
