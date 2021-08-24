4082eca762334623d87441360cf162ec1d664c3b
Bogdan Chadkin <trysound@yandex.ru>
2021-08-24 14:20:23 +0300

    Refactor cleanupEnableBackground with visitor plugin api (#1540)
    Slightly restructured plugin to do less work in case no filters used.

--- plugins/cleanupEnableBackground.js
+++ plugins/cleanupEnableBackground.js
@@ -1,13 +1,10 @@
 'use strict';
 
-const { traverse } = require('../lib/xast.js');
+const { visit } = require('../lib/xast.js');
 
+exports.type = 'visitor';
 exports.name = 'cleanupEnableBackground';
-
-exports.type = 'full';
-
 exports.active = true;
-
 exports.description =
   'remove or cleanup enable-background attribute when possible';
 
@@ -21,55 +18,58 @@ exports.description =
  *             ⬇
  * <svg width="100" height="50">
  *
- * @param {Object} root current iteration item
- * @return {Boolean} if false, item will be filtered out
- *
  * @author Kir Belevich
+ *
+ * @type {import('../lib/types').Plugin<void>}
  */
-exports.fn = function (root) {
+exports.fn = (root) => {
   const regEnableBackground =
     /^new\s0\s0\s([-+]?\d*\.?\d+([eE][-+]?\d+)?)\s([-+]?\d*\.?\d+([eE][-+]?\d+)?)$/;
+
   let hasFilter = false;
-  const elems = ['svg', 'mask', 'pattern'];
-
-  traverse(root, (node) => {
-    if (node.type === 'element') {
-      if (
-        elems.includes(node.name) &&
-        node.attributes['enable-background'] != null &&
-        node.attributes.width != null &&
-        node.attributes.height != null
-      ) {
-        const match =
-          node.attributes['enable-background'].match(regEnableBackground);
-
-        if (match) {
-          if (
-            node.attributes.width === match[1] &&
-            node.attributes.height === match[3]
-          ) {
-            if (node.name === 'svg') {
-              delete node.attributes['enable-background'];
-            } else {
-              node.attributes['enable-background'] = 'new';
-            }
-          }
+  visit(root, {
+    element: {
+      enter: (node) => {
+        if (node.name === 'filter') {
+          hasFilter = true;
         }
-      }
-      if (node.name === 'filter') {
-        hasFilter = true;
-      }
-    }
+      },
+    },
   });
 
-  if (hasFilter === false) {
-    traverse(root, (node) => {
-      if (node.type === 'element') {
-        //we don't need 'enable-background' if we have no filters
-        delete node.attributes['enable-background'];
-      }
-    });
-  }
-
-  return root;
+  return {
+    element: {
+      enter: (node) => {
+        if (node.attributes['enable-background'] == null) {
+          return;
+        }
+        if (hasFilter) {
+          if (
+            (node.name === 'svg' ||
+              node.name === 'mask' ||
+              node.name === 'pattern') &&
+            node.attributes.width != null &&
+            node.attributes.height != null
+          ) {
+            const match =
+              node.attributes['enable-background'].match(regEnableBackground);
+            if (
+              match != null &&
+              node.attributes.width === match[1] &&
+              node.attributes.height === match[3]
+            ) {
+              if (node.name === 'svg') {
+                delete node.attributes['enable-background'];
+              } else {
+                node.attributes['enable-background'] = 'new';
+              }
+            }
+          }
+        } else {
+          //we don't need 'enable-background' if we have no filters
+          delete node.attributes['enable-background'];
+        }
+      },
+    },
+  };
 };
