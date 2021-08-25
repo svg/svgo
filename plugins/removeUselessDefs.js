a228020329d0f6836c08034a09b5068bac217816
Bogdan Chadkin <trysound@yandex.ru>
2021-08-25 20:52:07 +0300

    Refactor defs plugins (#1544)
    Refactored and covered with types following plugins
    
    - removeUselessDefs
    - sortDefsChildren

--- plugins/removeUselessDefs.js
+++ plugins/removeUselessDefs.js
@@ -1,50 +1,65 @@
 'use strict';
 
-const { elemsGroups } = require('./_collections');
+/**
+ * @typedef {import('../lib/types').XastElement} XastElement
+ */
 
+const { detachNodeFromParent } = require('../lib/xast.js');
+const { elemsGroups } = require('./_collections.js');
+
+exports.type = 'visitor';
 exports.name = 'removeUselessDefs';
-
-exports.type = 'perItem';
-
 exports.active = true;
-
 exports.description = 'removes elements in <defs> without id';
 
 /**
  * Removes content of defs and properties that aren't rendered directly without ids.
  *
- * @param {Object} item current iteration item
- * @return {Boolean} if false, item will be filtered out
- *
  * @author Lev Solntsev
+ *
+ * @type {import('../lib/types').Plugin<void>}
  */
-exports.fn = function (item) {
-  if (item.type === 'element') {
-    if (item.name === 'defs') {
-      item.children = getUsefulItems(item, []);
-      if (item.children.length === 0) {
-        return false;
+exports.fn = () => {
+  return {
+    element: {
+      enter: (node, parentNode) => {
+        if (node.name === 'defs') {
+          /**
+           * @type {Array<XastElement>}
+           */
+          const usefulNodes = [];
+          collectUsefulNodes(node, usefulNodes);
+          if (usefulNodes.length === 0) {
+            detachNodeFromParent(node, parentNode);
+          }
+          // TODO remove in SVGO 3
+          for (const usefulNode of usefulNodes) {
+            // @ts-ignore parentNode is legacy
+            usefulNode.parentNode = node;
+          }
+          node.children = usefulNodes;
+        } else if (
+          elemsGroups.nonRendering.includes(node.name) &&
+          node.attributes.id == null
+        ) {
+          detachNodeFromParent(node, parentNode);
+        }
+      },
+    },
+  };
+};
+
+/**
+ * @type {(node: XastElement, usefulNodes: Array<XastElement>) => void}
+ */
+const collectUsefulNodes = (node, usefulNodes) => {
+  for (const child of node.children) {
+    if (child.type === 'element') {
+      if (child.attributes.id != null || child.name === 'style') {
+        usefulNodes.push(child);
+      } else {
+        collectUsefulNodes(child, usefulNodes);
       }
-    } else if (
-      elemsGroups.nonRendering.includes(item.name) &&
-      item.attributes.id == null
-    ) {
-      return false;
     }
   }
 };
-
-function getUsefulItems(item, usefulItems) {
-  for (const child of item.children) {
-    if (child.type === 'element') {
-      if (child.attributes.id != null || child.name === 'style') {
-        usefulItems.push(child);
-        child.parentNode = item;
-      } else {
-        child.children = getUsefulItems(child, usefulItems);
-      }
-    }
-  }
-
-  return usefulItems;
-}
