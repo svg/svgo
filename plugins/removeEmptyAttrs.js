4377ea38c419fba373acef2eb56f6f7a21a77dda
Bogdan Chadkin <trysound@yandex.ru>
2021-10-07 14:07:06 +0300

    Refactor removeEmptyAttrs (#1594)
    - migrated to visitor plugin api
    - covered with tsdoc

--- plugins/removeEmptyAttrs.js
+++ plugins/removeEmptyAttrs.js
@@ -2,32 +2,32 @@
 
 const { attrsGroups } = require('./_collections.js');
 
+exports.type = 'visitor';
 exports.name = 'removeEmptyAttrs';
-
-exports.type = 'perItem';
-
 exports.active = true;
-
 exports.description = 'removes empty attributes';
 
 /**
  * Remove attributes with empty values.
  *
- * @param {Object} item current iteration item
- * @return {Boolean} if false, item will be filtered out
- *
  * @author Kir Belevich
+ *
+ * @type {import('../lib/types').Plugin<void>}
  */
-exports.fn = function (item) {
-  if (item.type === 'element') {
-    for (const [name, value] of Object.entries(item.attributes)) {
-      if (
-        value === '' &&
-        // empty conditional processing attributes prevents elements from rendering
-        attrsGroups.conditionalProcessing.includes(name) === false
-      ) {
-        delete item.attributes[name];
-      }
-    }
-  }
+exports.fn = () => {
+  return {
+    element: {
+      enter: (node) => {
+        for (const [name, value] of Object.entries(node.attributes)) {
+          if (
+            value === '' &&
+            // empty conditional processing attributes prevents elements from rendering
+            attrsGroups.conditionalProcessing.includes(name) === false
+          ) {
+            delete node.attributes[name];
+          }
+        }
+      },
+    },
+  };
 };
