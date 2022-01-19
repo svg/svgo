aaf1cfa5e7647d68a14c62ffbc14574b1c7ed3ec
Bogdan Chadkin <trysound@yandex.ru>
2022-01-19 20:42:13 +0300

    Refactor removeNonInheritableGroupAttrs and moveGroupAttrsToElems (#1636)
    Batched in one PR. Pretty simple changes. Just migrated to api and
    added more safety checks for types.

--- plugins/removeNonInheritableGroupAttrs.js
+++ plugins/removeNonInheritableGroupAttrs.js
@@ -1,38 +1,40 @@
 'use strict';
 
-exports.name = 'removeNonInheritableGroupAttrs';
-
-exports.type = 'perItem';
-
-exports.active = true;
-
-exports.description =
-  'removes non-inheritable group’s presentational attributes';
-
 const {
   inheritableAttrs,
   attrsGroups,
   presentationNonInheritableGroupAttrs,
 } = require('./_collections');
 
+exports.type = 'visitor';
+exports.name = 'removeNonInheritableGroupAttrs';
+exports.active = true;
+exports.description =
+  'removes non-inheritable group’s presentational attributes';
+
 /**
  * Remove non-inheritable group's "presentation" attributes.
  *
- * @param {Object} item current iteration item
- * @return {Boolean} if false, item will be filtered out
- *
  * @author Kir Belevich
+ *
+ * @type {import('../lib/types').Plugin<void>}
  */
-exports.fn = function (item) {
-  if (item.type === 'element' && item.name === 'g') {
-    for (const name of Object.keys(item.attributes)) {
-      if (
-        attrsGroups.presentation.includes(name) === true &&
-        inheritableAttrs.includes(name) === false &&
-        presentationNonInheritableGroupAttrs.includes(name) === false
-      ) {
-        delete item.attributes[name];
-      }
-    }
-  }
+exports.fn = () => {
+  return {
+    element: {
+      enter: (node) => {
+        if (node.name === 'g') {
+          for (const name of Object.keys(node.attributes)) {
+            if (
+              attrsGroups.presentation.includes(name) === true &&
+              inheritableAttrs.includes(name) === false &&
+              presentationNonInheritableGroupAttrs.includes(name) === false
+            ) {
+              delete node.attributes[name];
+            }
+          }
+        }
+      },
+    },
+  };
 };
