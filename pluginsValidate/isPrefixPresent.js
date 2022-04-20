1394d8608fff06fa82bafc934f82c04bb2560e31
tymon <01158999@pw.edu.pl>
2022-04-20 10:40:54 +0200

    change: change logo and illustration suffix logic

--- pluginsValidate/isPrefixPresent.js
+++ pluginsValidate/isPrefixPresent.js
@@ -4,7 +4,7 @@ exports.type = 'validate';
 
 exports.active = false;
 
-exports.description = 'checks if the filename has prefix';
+exports.description = 'checks if the filename has suffix';
 
 /**
  * Checks if the file is snake_case named
@@ -19,19 +19,42 @@ exports.description = 'checks if the filename has prefix';
  * @author Tymon Żarski
  */
 
-exports.fn = function (root, validateResult) {
-  if (root.filename) {
-    const filename = root.filename;
-    const prefixes = [
-      'retail_',
-      'corporate_',
-      'sme_',
-      'privatebanking_',
-      'mobile_',
-    ];
-    const result = prefixes.some((prefix) => filename.indexOf(prefix) === 0);
+const ENOCLS = `Error in plugin "isPrefixPresent": absent parameters.
+ It should have a asset type in "assetType".
+ Config example:
 
-    validateResult.isPrefixPresent = result;
+ plugins:
+ - isPrefixPresent:
+     assetType: 'logo' || 'illustration' // set asset type to 'logo' or 'illustration'
+ `;
+
+exports.fn = function (root, validateResult, params) {
+  if (root.filename) {
+    const assetType = params.assetType;
+
+    if (assetType !== 'logo' && assetType !== 'illustration') {
+      console.log(ENOCLS);
+    }
+
+    const filename = root.filename.replace(/\.[^/.]+$/, '');
+    const darkThemSuffix = '_dark_theme';
+    const themeSuffixes = ['_retail', '_corporate', '_sme', '_mobile'];
+
+    themeSuffixes.push(assetType === 'logo' ? '_private' : '_private_banking');
+
+    const isDarkThemSuffix =
+      filename.indexOf(darkThemSuffix) ===
+        filename.length - darkThemSuffix.length ||
+      filename.indexOf(darkThemSuffix) === -1;
+
+    const isThemePrefix = themeSuffixes.some((prefix) => {
+      return filename.indexOf(prefix) !== -1 &&
+        filename.indexOf(prefix) === filename.length + isDarkThemSuffix
+        ? darkThemSuffix.length
+        : null - prefix.length && filename.indexOf(prefix) > 0;
+    });
+
+    validateResult.isPrefixPresent = isDarkThemSuffix && isThemePrefix;
   } else {
     validateResult.isPrefixPresent = false;
     console.log('no filename provided!');
