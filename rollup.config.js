f2094764b9e149352ae838f80c725a8aaeae1b1e
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2022-09-11 14:39:46 +0200

    chore: Use esbuild for building library

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import esbuild from 'rollup-plugin-esbuild';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
@@ -20,6 +22,30 @@ export default {
       compress: false,
       mangle: false,
       format: { comments: false },
+    }),
+    esbuild({
+      // All options are optional
+      include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+      exclude: /node_modules/, // default
+      sourceMap: true, // default
+      minify: process.env.NODE_ENV === 'production',
+      target: 'es2017', // default, or 'es20XX', 'esnext'
+      jsx: 'transform', // default, or 'preserve'
+      jsxFactory: 'React.createElement',
+      jsxFragment: 'React.Fragment',
+      // Like @rollup/plugin-replace
+      define: {
+        __VERSION__: '"x.y.z"',
+      },
+      tsconfig: 'tsconfig.json', // default
+      // Add extra loaders
+      loaders: {
+        // Add .json files support
+        // require @rollup/plugin-commonjs
+        '.json': 'json',
+        // Enable JSX in .js files too
+        '.js': 'jsx',
+      },
     }),
   ],
 };
