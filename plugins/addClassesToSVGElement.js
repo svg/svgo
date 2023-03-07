2f6c4598d3302b70d99de61ff4677c96456515f3
Ans <anselmbradford@users.noreply.github.com>
2023-03-07 14:23:00 -0500

    Add suffixPattern parameter to addClassesToSVGElement plugin
    This PR adds a `suffixPattern` parameter to the `addClassesToSVGElement` plugin, which allows adding of a suffix to the custom classes added on the SVG element.

--- plugins/addClassesToSVGElement.js
+++ plugins/addClassesToSVGElement.js
@@ -1,12 +1,13 @@
 'use strict';
 
+const path = require('path');
+
 exports.name = 'addClassesToSVGElement';
 exports.description = 'adds classnames to an outer <svg> element';
 
 var ENOCLS = `Error in plugin "addClassesToSVGElement": absent parameters.
 It should have a list of classes in "classNames" or one "className".
 Config example:
-
 plugins: [
   {
     name: "addClassesToSVGElement",
@@ -15,7 +16,6 @@ plugins: [
     }
   }
 ]
-
 plugins: [
   {
     name: "addClassesToSVGElement",
@@ -51,7 +51,7 @@ plugins: [
  *
  * @type {import('./plugins-types').Plugin<'addClassesToSVGElement'>}
  */
-exports.fn = (root, params) => {
+exports.fn = (root, params, info) => {
   if (
     !(Array.isArray(params.classNames) && params.classNames.some(String)) &&
     !params.className
@@ -60,6 +60,26 @@ exports.fn = (root, params) => {
     return null;
   }
   const classNames = params.classNames || [params.className];
+
+  /**
+   * If we have a suffix pattern,
+   * duplicate the existing class names and add a copy with the suffix.
+   * If the suffix pattern contains the magic keyword '$FILENAME',
+   * it is replaced with the actual filename. For example,
+   * if the className param is 'mySvg' and the suffixPattern
+   * param is '__$FILENAME`, then for 'cat.svg`, the final classes
+   * will be 'mySvg mySvg__cat'. For 'dog.svg', the final classes
+   * will be 'mySvg mSvg__dog'.
+   */
+  if (typeof params.suffixPattern !== 'undefined') {
+    let suffixPattern = params.suffixPattern;
+    const filename = path.basename(info.path, '.svg');
+    suffixPattern = suffixPattern.replace('$FILENAME', filename);
+    for (var i = 0, len = classNames.length; i < len; i++) {
+      classNames.push(classNames[i] + suffixPattern);
+    }
+  }
+
   return {
     element: {
       enter: (node, parentNode) => {
