e71c4512d7a85ed400fe316a75d8e087b2a40676
Jakub Filipowski <filipowski.kuba@gmail.com>
2023-09-15 16:29:03 +0200

    fix: check if darkmodemask element is present in isDarkmodeMaskCorrect rule

--- pluginsValidate/isDarkmodeMaskCorrect.js
+++ pluginsValidate/isDarkmodeMaskCorrect.js
@@ -38,64 +38,70 @@ const ENOCLS = `Error in plugin "isDarkmodeMaskCorrect": absent parameters.
 exports.fn = function (root, validateResult, params) {
   if (params || !utils.isEmpty(params)) {
     const svg = utils.findElementByName(root, 'svg');
-    const darkmodeMaskElement = utils.findAllElementByAttributeValue(
+    const darkmodeMaskElements = utils.findAllElementByAttributeValue(
       svg,
       'id',
       'darkmodeMask'
-    )[0];
-
-    const [darkmodeFillWidth, darkmodeFillHeight] = params.darkmodeFillSize;
+    );
 
     let isFillColorCorrect = false;
-
-    for (const color of params.fillColors) {
-      if (
-        utils.findAllElementByAttributeValue(darkmodeMaskElement, 'fill', color)
-          .length > 0 ||
-        darkmodeMaskElement.attributes.fill === color
-      ) {
-        isFillColorCorrect = true;
-      }
-    }
-
     let isCorrectSize;
-    let elementWidth, elementHeight;
-    if (darkmodeMaskElement.name === 'path') {
-      [elementWidth, elementHeight] = utils.calculatePathDimensions(
-        darkmodeMaskElement.attributes.d
-      );
-    } else if (darkmodeMaskElement.name === 'g') {
-      const pathElem = darkmodeMaskElement.children.find(
-        (child) => child.name === 'path'
-      );
 
-      if (pathElem) {
+    if (darkmodeMaskElements.length > 0) {
+      const darkmodeMaskElement = darkmodeMaskElements[0];
+
+      const [darkmodeFillWidth, darkmodeFillHeight] = params.darkmodeFillSize;
+
+      for (const color of params.fillColors) {
+        if (
+          utils.findAllElementByAttributeValue(
+            darkmodeMaskElement,
+            'fill',
+            color
+          ).length > 0 ||
+          darkmodeMaskElement.attributes.fill === color
+        ) {
+          isFillColorCorrect = true;
+        }
+      }
+
+      let elementWidth, elementHeight;
+      if (darkmodeMaskElement.name === 'path') {
         [elementWidth, elementHeight] = utils.calculatePathDimensions(
-          pathElem.attributes.d
+          darkmodeMaskElement.attributes.d
+        );
+      } else if (darkmodeMaskElement.name === 'g') {
+        const pathElem = darkmodeMaskElement.children.find(
+          (child) => child.name === 'path'
+        );
+
+        if (pathElem) {
+          [elementWidth, elementHeight] = utils.calculatePathDimensions(
+            pathElem.attributes.d
+          );
+        }
+      } else if (darkmodeMaskElement.name === 'ellipse') {
+        [elementWidth, elementHeight] = [
+          darkmodeMaskElement.attributes.rx * 2,
+          darkmodeMaskElement.attributes.ry * 2,
+        ];
+      } else if (darkmodeMaskElement.name === 'circle') {
+        [elementWidth, elementHeight] = [
+          darkmodeMaskElement.attributes.r * 2,
+          darkmodeMaskElement.attributes.r * 2,
+        ];
+      } else {
+        console.error(
+          'Error in plugin isDarkmodeMaskCorrect: Unhandled element found!'
         );
       }
-    } else if (darkmodeMaskElement.name === 'ellipse') {
-      [elementWidth, elementHeight] = [
-        darkmodeMaskElement.attributes.rx * 2,
-        darkmodeMaskElement.attributes.ry * 2,
-      ];
-    } else if (darkmodeMaskElement.name === 'circle') {
-      [elementWidth, elementHeight] = [
-        darkmodeMaskElement.attributes.r * 2,
-        darkmodeMaskElement.attributes.r * 2,
-      ];
-    } else {
-      console.error(
-        'Error in plugin isDarkmodeMaskCorrect: Unhandled element found!'
-      );
-    }
 
-    if (elementWidth !== elementHeight) isCorrectSize = false;
-    else
-      isCorrectSize =
-        elementWidth === darkmodeFillWidth &&
-        elementHeight === darkmodeFillHeight;
-
+      if (elementWidth !== elementHeight) isCorrectSize = false;
+      else
+        isCorrectSize =
+          elementWidth === darkmodeFillWidth &&
+          elementHeight === darkmodeFillHeight;
+    } else isCorrectSize = false;
     const result = isFillColorCorrect && isCorrectSize;
 
     validateResult.isDarkmodeMaskCorrect = result;
