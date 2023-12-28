f6a2ca20d6472cb0f2b18dadd626d6db092f8767
Jon Dufresne <jon.dufresne@gmail.com>
2023-12-28 14:14:47 -0500

    chore: improve jest tests with more precise assertions (#1912)

--- test/svgo/_index.test.js
+++ test/svgo/_index.test.js
@@ -24,16 +24,16 @@ describe('svgo', () => {
       plugins: [],
       js2svg: { pretty: true, indent: 2 },
     });
-    expect(normalize(result.data)).toEqual(expected);
+    expect(normalize(result.data)).toStrictEqual(expected);
   });
   it('should handle plugins order properly', async () => {
     const [original, expected] = await parseFixture('plugins-order.svg');
     const result = optimize(original, { path: 'input.svg' });
-    expect(normalize(result.data)).toEqual(expected);
+    expect(normalize(result.data)).toStrictEqual(expected);
   });
   it('should handle empty svg tag', async () => {
     const result = optimize('<svg />', { path: 'input.svg' });
-    expect(result.data).toEqual('<svg/>');
+    expect(result.data).toBe('<svg/>');
   });
   it('should preserve style specificity over attributes', async () => {
     const [original, expected] = await parseFixture('style-specificity.svg');
@@ -41,7 +41,7 @@ describe('svgo', () => {
       path: 'input.svg',
       js2svg: { pretty: true },
     });
-    expect(normalize(result.data)).toEqual(expected);
+    expect(normalize(result.data)).toStrictEqual(expected);
   });
   it('should inline entities', async () => {
     const [original, expected] = await parseFixture('entities.svg');
@@ -50,7 +50,7 @@ describe('svgo', () => {
       plugins: [],
       js2svg: { pretty: true },
     });
-    expect(normalize(result.data)).toEqual(expected);
+    expect(normalize(result.data)).toStrictEqual(expected);
   });
   it('should preserve whitespaces between tspan tags', async () => {
     const [original, expected] = await parseFixture('whitespaces.svg');
@@ -58,7 +58,7 @@ describe('svgo', () => {
       path: 'input.svg',
       js2svg: { pretty: true },
     });
-    expect(normalize(result.data)).toEqual(expected);
+    expect(normalize(result.data)).toStrictEqual(expected);
   });
   it('should preserve "to" keyframe selector', async () => {
     const [original, expected] = await parseFixture('keyframe-selectors.svg');
@@ -66,14 +66,14 @@ describe('svgo', () => {
       path: 'input.svg',
       js2svg: { pretty: true },
     });
-    expect(normalize(result.data)).toEqual(expected);
+    expect(normalize(result.data)).toStrictEqual(expected);
   });
   it('should not trim whitespace at start and end of pre element', async () => {
     const [original, expected] = await parseFixture('pre-element.svg');
     const result = optimize(original, {
       path: 'input.svg',
     });
-    expect(normalize(result.data)).toEqual(expected);
+    expect(normalize(result.data)).toStrictEqual(expected);
   });
   it('should not add whitespace in pre element', async () => {
     const [original, expected] = await parseFixture('pre-element-pretty.svg');
@@ -81,6 +81,6 @@ describe('svgo', () => {
       path: 'input.svg',
       js2svg: { pretty: true },
     });
-    expect(normalize(result.data)).toEqual(expected);
+    expect(normalize(result.data)).toStrictEqual(expected);
   });
 });
