eabc29d159dd82bb70a6a1de5ea3c4b478dd67bb
Seth Falco <seth@falco.fun>
2025-05-06 08:05:09 +0100

    fix: types for loadConfig and add more tests (#2123)

--- test-d/lib/svgo-node.test-d.ts
+++ test-d/lib/svgo-node.test-d.ts
@@ -1,5 +1,16 @@
 import { expectType, expectAssignable } from 'tsd';
-import { DataUri, Output, optimize } from '../../types/lib/svgo-node.js';
+import {
+  type Config,
+  type DataUri,
+  type Output,
+  loadConfig,
+  optimize,
+} from '../../types/lib/svgo-node.js';
 
 expectType<Output>(optimize('<svg></svg>'));
 expectAssignable<DataUri>('enc');
+
+expectType<Promise<Config | null>>(loadConfig());
+expectType<Promise<Config | null>>(loadConfig(undefined));
+expectType<Promise<Config | null>>(loadConfig(null));
+expectType<Promise<Config>>(loadConfig('svgo.config.js'));
