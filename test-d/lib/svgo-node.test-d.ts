d85834f63a5e518f83919f987b0ecfb6801844f5
Seth Falco <seth@falco.fun>
2025-10-11 23:27:42 +0100

    types: clearer types for presets

--- test-d/lib/svgo-node.test-d.ts
+++ test-d/lib/svgo-node.test-d.ts
@@ -1,8 +1,10 @@
 import { expectType, expectAssignable } from 'tsd';
 import {
+  BuiltinPlugin,
   type Config,
   type DataUri,
   type Output,
+  builtinPlugins,
   loadConfig,
   optimize,
 } from '../../types/lib/svgo-node.js';
@@ -14,3 +16,13 @@ expectType<Promise<Config | null>>(loadConfig());
 expectType<Promise<Config | null>>(loadConfig(undefined));
 expectType<Promise<Config | null>>(loadConfig(null));
 expectType<Promise<Config>>(loadConfig('svgo.config.js'));
+
+const presetDefault = builtinPlugins.find(
+  (plugin) => plugin.name === 'preset-default',
+)!;
+if (!presetDefault.isPreset) {
+  throw Error('Could not find preset-default.');
+}
+
+expectType<ReadonlyArray<BuiltinPlugin<string, Object>>>(presetDefault.plugins);
+expectType<'preset-default'>(presetDefault.name);
