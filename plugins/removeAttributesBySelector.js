686487258ccace05ba877ff19be4316a88327d57
Nicholas Narsing <soren121@sorenstudios.com>
2026-04-07 11:41:35 -0700

    fix(removeAttributesBySelector): update for v2, catch missing parameters, update docs

--- plugins/removeAttributesBySelector.js
+++ plugins/removeAttributesBySelector.js
@@ -1,8 +1,30 @@
-import { querySelectorAll } from '../lib/xast.js';
+import { matches } from '../lib/xast.js';
+
+/**
+ * @typedef AttributesBySelector
+ * @property {string} selector
+ * @property {string | string[]} attributes
+ * @property {never} selectors
+ */
+
+/**
+ * @typedef AttributesBySelectors
+ * @property {never} selector
+ * @property {never} attributes
+ * @property {Array<AttributesBySelector>} selectors
+ */
+
+/**
+ * @typedef {(AttributesBySelector | AttributesBySelectors)} RemoveAttributesBySelectorParams
+ */
 
 export const name = 'removeAttributesBySelector';
 export const description =
   'removes attributes of elements that match a css selector';
+
+const ENOATTRS = `Warning: The plugin "removeAttributesBySelector" is missing parameters.
+It should have a list of "selectors", or one "selector" and one "attributes".
+Without either, the plugin is a noop.`;
 
 /**
  * Removes attributes of elements that match a css selector.
@@ -67,29 +89,40 @@ export const description =
  *   ↓
  * <rect x="0" y="0" width="100" height="100"/>
  *
- * @link https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors|MDN CSS Selectors
+ * @link https://developer.mozilla.org/docs/Web/CSS/CSS_Selectors|MDN CSS Selectors
  *
  * @author Bradley Mease
  *
- * @type {import('../lib/types.js').Plugin<any>}
+ * @type {import('../lib/types.js').Plugin<RemoveAttributesBySelectorParams>}
  */
 export const fn = (root, params) => {
+  if (
+    !Array.isArray(params.selectors) &&
+    (!params.selector || !params.attributes)
+  ) {
+    console.warn(ENOATTRS);
+    return null;
+  }
+
   const selectors = Array.isArray(params.selectors)
     ? params.selectors
     : [params];
-  for (const { selector, attributes } of selectors) {
-    const nodes = querySelectorAll(root, selector);
-    for (const node of nodes) {
-      if (node.type === 'element') {
-        if (Array.isArray(attributes)) {
-          for (const name of attributes) {
-            delete node.attributes[name];
+
+  return {
+    element: {
+      enter: (node) => {
+        for (const { selector, attributes } of selectors) {
+          if (matches(node, selector)) {
+            if (Array.isArray(attributes)) {
+              for (const name of attributes) {
+                delete node.attributes[name];
+              }
+            } else {
+              delete node.attributes[attributes];
+            }
           }
-        } else {
-          delete node.attributes[attributes];
         }
-      }
-    }
-  }
-  return {};
+      },
+    },
+  };
 };
