2523117d54b6a3ce112a98285d98afeef8f88cfd
Neal Lin <21194575+Neallin-917@users.noreply.github.com>
2026-08-12 10:14:32 +0800

    fix: match CSS attribute selector values

--- plugins/inlineStyles.js
+++ plugins/inlineStyles.js
@@ -261,7 +261,11 @@ export const fn = (root, params) => {
                 if (
                   attrsGroups.presentation.has(property) &&
                   !selectors.some((selector) =>
-                    includesAttrSelector(selector.item, property),
+                    includesAttrSelector(
+                      selector.item,
+                      property,
+                      selectedEl.attributes[property],
+                    ),
                   )
                 ) {
                   delete selectedEl.attributes[property];
